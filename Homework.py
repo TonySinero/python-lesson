9dbc250de8c59f4cdde9834b8636e5008eb19f7b
Tony <tadmopar@gmail,com>
2021-02-16 20:31:22 +0300

    first commit

---
+++ Homework.py
@@ -0,0 +1,34 @@
+import math
+# a = 15
+# b = 22
+# c = a + b
+# z = a - b
+# x = a * b
+# print(c)
+# print(z)
+# print(x)
+# #--------------------------------
+# x = 5
+# y = 6
+# c = abs(x) - abs(y)/1 + abs(x*y)
+# print(c)
+# #----------------------------------
+# lengh_cube = 7
+# side = lengh_cube * lengh_cube
+# size = lengh_cube * 3
+# print(size)
+# print(side)
+# #----------------------------------
+# x = 4
+# b = 5
+# c = x + b/2
+# z = math.sqrt(x*b)
+# print(c)
+# print(z)
+# #----------------------------------
+# g = 8
+# e = 3
+# f = g * e/2
+# c = math.sqrt(g**2 + e**2)
+# print(f)
+# print(c)
