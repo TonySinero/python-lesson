e3e18adbf4580eab99caefeef0d7c1326c687e13
Tony <tadmopar@gmail,com>
2021-02-18 20:03:52 +0300

    Second test

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-'the firs one'
+def second(first , second ):
+    while True:
+        ink = input(first)
+        if ink == 'q':
+            break
+        inv = input(second)
+        if inv == 'q':
+            break
+        return second
+
+    cin = second(inv,ink)
