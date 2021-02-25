1b5cef2958b221329b08915964be817da3640cff
Tony <tadmopar@gmail,com>
2021-02-25 19:00:39 +0300

    redisine

--- task_4_4.py
+++ task_4_4.py
@@ -1,16 +1,17 @@
-# lis = [1, 2, 3, 4, 5, 6 ]
-# for x in lis:
-#     x = lis[1:]+lis[:1]
-#
-# print(x)
+lis = [1, 2, 3, 4, 5, 6 ]
+for x in lis:
+    x = lis[1:]+lis[:1]
+
+print(x)
 #--------------------------
 
 
 a = [1, 2, 3, 4, 5, 6]
 b = []
 i = 0
-while i < len(a):
+while a:
     x = a[1:] + [0]
-    b.append(x)
+    b.extend(x)
     i += 1
+    break
 print(b)
